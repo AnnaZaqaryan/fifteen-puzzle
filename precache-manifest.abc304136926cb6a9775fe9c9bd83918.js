07d2ea18c558e47fdabf128b071671880fd4e87c
Anna Zaqaryan <anahit.zakaryan1992@gmail.com>
2019-11-14 22:44:13 +0400

    Updates

---
+++ precache-manifest.abc304136926cb6a9775fe9c9bd83918.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd65693c228c8dbc52d9a23cc584116",
+    "url": "/fifteen-puzzle/index.html"
+  },
+  {
+    "revision": "638825200f85d6488942",
+    "url": "/fifteen-puzzle/static/css/main.53981119.chunk.css"
+  },
+  {
+    "revision": "1a0a6b13e460afa51b60",
+    "url": "/fifteen-puzzle/static/js/2.d28a570b.chunk.js"
+  },
+  {
+    "revision": "638825200f85d6488942",
+    "url": "/fifteen-puzzle/static/js/main.cd99a3da.chunk.js"
+  },
+  {
+    "revision": "51b5ee5bda0e492653cf",
+    "url": "/fifteen-puzzle/static/js/runtime-main.05b9d7d5.js"
+  }
+]);
